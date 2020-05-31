e278c7a2e2c835d62d78341cb1dc52497328126e
JoaoColeoni <60195895+JoaoColeoni@users.noreply.github.com>
2020-05-30 22:18:38 -0300

    Screens Novos

---
+++ Assets/Scripts/Manager/SelectRoomManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectRoomManager : MonoBehaviour
+{
+    public static SelectRoomManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    [Header("Rooms")]
+    public GameObject m_RoomButton;
+    public GameObject m_RoomContent;
+    public List<Room> m_rooms = new List<Room>();
+
+    private void Start()
+    {
+        foreach (Room room in m_rooms)
+        {
+            GameObject button = Instantiate(m_RoomButton);
+            button.transform.parent = m_RoomContent.transform;
+            button.transform.localScale = Vector3.one;
+
+            var script = button.GetComponent<RoomButton>();
+            script.SetRoom(room);
+        }
+    }
+}
+
+[System.Serializable]
+public class Room
+{
+    public string room_name;
+    public int amount;
+    public int max_size;
+}
---
+++ Assets/Scripts/RoomButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomButton : MonoBehaviour
+{
+    public Text m_TextUI;
+    public Text m_Amount;
+    public Room m_Room;
+    private Button m_Button;
+
+    private void Awake()
+    {
+        m_Button = GetComponent<Button>();
+    }
+
+    public void SetRoom(Room room)
+    {
+        m_Room = room;
+        m_TextUI.text = room.room_name;
+        m_Amount.text = room.amount.ToString() + "/" + room.max_size.ToString();
+        // m_Button.onClick.AddListener(() => ShowDetails());
+    }
+}
+

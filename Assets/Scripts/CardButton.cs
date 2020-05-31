83ca2e8e64f2720a0bb487224b1b113c28e0db94
JoaoColeoni <60195895+JoaoColeoni@users.noreply.github.com>
2020-05-31 16:33:03 -0300

    New Scripts

---
+++ Assets/Scripts/CardButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardButton : MonoBehaviour
+{
+    public Text m_TextUI;
+    public Card m_Card;
+    private Button m_Button;
+
+    private void Awake()
+    {
+        m_Button = GetComponent<Button>();
+    }
+
+    public void SetCard(Card card)
+    {
+        m_Card = card;
+        m_TextUI.text = card.label;
+        // m_Button.onClick.AddListener(() => ShowDetails());
+    }
+}
+
---
+++ Assets/Scripts/Manager/CardManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardManager : MonoBehaviour
+{
+    public static CardManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    [Header("Cards")]
+    public GameObject m_CardButton;
+    public List<Card> m_Cards = new List<Card>();
+    public GameObject m_CardParent;
+
+    private void Start()
+    {
+        foreach (Card card in m_Cards)
+        {
+            GameObject button = Instantiate(m_CardButton);
+            button.transform.parent = m_CardParent.transform;
+            button.transform.localScale = Vector3.one;
+
+            var script = button.GetComponent<CardButton>();
+            script.SetCard(card);
+        }
+    }
+}
+
+[System.Serializable]
+public class Card
+{
+    public string label;
+}

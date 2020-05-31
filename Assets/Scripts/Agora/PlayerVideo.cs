111508ff0f608a09e0c67332194926811a2b0714
Gabriel Santini Quibao <gquibao@gmail.com>
2020-05-31 02:41:25 -0300

    Adding video call functionalities using Agora io. Fixing issues related to it

---
+++ Assets/Scripts/Agora/PlayerVideo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using agora_gaming_rtc;
+using UnityEngine;
+
+public class PlayerVideo : MonoBehaviour
+{
+    public GameObject prefab;
+    public VideoSurface videoSurface;
+
+    public void Set(uint uid)
+    {
+        videoSurface.gameObject.SetActive(true);
+        videoSurface.SetForUser(uid);
+        videoSurface.SetGameFps(60);
+        videoSurface.SetEnable(true);
+    }
+
+    public void Clear()
+    {
+        videoSurface.SetEnable(false);
+        videoSurface.gameObject.SetActive(false);
+    }
+}

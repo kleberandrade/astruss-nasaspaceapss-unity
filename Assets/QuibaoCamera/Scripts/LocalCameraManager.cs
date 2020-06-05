17dab7702097830c07de0bc21fdbe0b91ea683b8
Gabriel Santini Quibao <gquibao@gmail.com>
2020-06-05 00:59:59 -0300

    Added script to manage microphone input

---
+++ Assets/QuibaoCamera/Scripts/LocalCameraManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class LocalCameraManager : MonoBehaviour
+{
+    private WebCamTexture _webCamTexture = null;
+    private WebCamDevice[] _webCamDevices;
+    private Renderer _renderer;
+ 
+    IEnumerator Start ()
+    {
+        if(!Permission.HasUserAuthorizedPermission(Permission.Camera))
+            Permission.RequestUserPermission(Permission.Camera);
+        
+        while(!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            yield return null;
+        }
+
+        _renderer = GetComponent<Renderer>();
+        _webCamDevices = WebCamTexture.devices;
+        _webCamDevices.ToList().ForEach(webCamDevice =>
+        {
+            if (webCamDevice.isFrontFacing)
+            {
+                _webCamTexture = new WebCamTexture(webCamDevice.name);
+            }
+        });
+        _renderer.material.mainTexture = _webCamTexture;
+        _webCamTexture.Play();
+    }
+}
---
+++ Assets/QuibaoCamera/Scripts/LocalMicrophoneManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.UI;
+
+public class LocalMicrophoneManager : MonoBehaviour
+{
+    public AudioSource localAudio;
+    private IEnumerator Start()
+    {
+        if(!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+            Permission.RequestUserPermission(Permission.Microphone);
+        
+        while(!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            yield return null;
+        }
+        
+        localAudio.clip = Microphone.Start(Microphone.devices[0], true, 10, 44100);
+        localAudio.loop = true;
+        while (!(Microphone.GetPosition(null) > 0))
+        {
+            yield return null;
+        }
+        localAudio.Play();
+    }
+}
